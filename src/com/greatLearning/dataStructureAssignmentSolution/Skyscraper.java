0d751a58658f5c16f67740e0a7a7d642cb9d1a0c
abdurrahman21 <abdurrahman.md21@gmail.com>
2022-08-07 23:47:06 +0530

    skyscraper related changes

--- src/com/greatLearning/dataStructureAssignmentSolution/Skyscraper.java
+++ src/com/greatLearning/dataStructureAssignmentSolution/Skyscraper.java
@@ -1,22 +1,34 @@
 package com.greatLearning.dataStructureAssignmentSolution;
 
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Skyscraper {
 
-	public static Queue<Integer> sortstack(Queue<Integer> input) {
-		Queue<Integer> tmpStack = new PriorityQueue<Integer>();
+	public static Stack<Integer> sortstack(Queue<Integer> input) {
+		Stack<Integer> tmpStack = new Stack<Integer>();
 		while (!input.isEmpty()) {
 			int tmp = input.remove();
 			while (!tmpStack.isEmpty() && tmpStack.peek() > tmp) {
-				input.add(tmpStack.remove());
+				input.add(tmpStack.pop());
 			}
 
 			tmpStack.add(tmp);
 		}
 		return tmpStack;
+	}
+	
+	public static void check(Stack<Integer> stackArr) {
+		int j = 0;
+		while(!stackArr.isEmpty()) {
+			int count = j + 1;
+			if(stackArr.get(j) > stackArr.get(j + 1)) {
+				System.out.println("Day " + count + ":" + stackArr.get(j));
+				j++;
+			}
+			stackArr.remove(j);
+		}
 	}
 
 	public static void main(String args[]) {
@@ -27,7 +39,7 @@ public class Skyscraper {
 		System.out.println("enter the total no of floors in the building : ");
 		no_of_Floors = sc.nextInt();
 
-		Queue<Integer> queueInt = new PriorityQueue<Integer>();
+		Stack<Integer> stackInt = new Stack<Integer>();
 
 		int[] floorSize = new int[no_of_Floors];
 
@@ -35,10 +47,15 @@ public class Skyscraper {
 			int count = i + 1;
 			System.out.println("enter the floor size given on day : " + count);
 			floorSize[i] = sc.nextInt();
-			queueInt.add(floorSize[i]);
+			stackInt.add(floorSize[i]);
 		}
 
-		System.out.println(sortstack(queueInt));
+		//System.out.println(sortstack(queueInt));
+		for (int i = 0; i < no_of_Floors; i++) {
+			check(stackInt);
+		}
+		
+		
 
 	}
 
